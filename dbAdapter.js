c6ef31e7d002435387d2e001c5728e0a23a7e0a2
Ariel Shil <arielshil23@gmail.com>
2022-05-29 13:57:57 +0300

    feat(mongodb): add insert func

--- dbAdapter.js
+++ dbAdapter.js
@@ -1,1 +1,15 @@
-/// text 1 db Tal Kahalon :) 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://0.0.0.0:27017/";
+
+const insertDB = function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("HumanResouceDB");
+    var myobj = { name: "Tal Kahalon", empid: "123456" };
+    dbo.collection("projectDB").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }
+
+MongoClient.connect(url, insertDB );
